45601fb33b6b71de01add74b3d0aedd03cfe8a48
Jody Clements <jody@neomorphic.co.uk>
2015-01-13 14:02:49 -0500

    Changed localhost port for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -143,7 +143,7 @@ module.exports = function(grunt) {
         connect: {
             server: {
                 options: {
-                    port: 8000,
+                    port: 8100,
                     base: "."
                 }
             }
